ef88d6958bfbd28d89a4191ca76e1a499c80eba7
Jiongxuan <875385920@qq.com>
2019-08-19 22:21:51 +0800

    Add config files for AWS Developer Tools

---
+++ scripts/npm-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+chmod +x /home/ec2-user/scripts/start_server.sh
+chmod +x /home/ec2-user/scripts/stop_server.sh
+npm install
+npm install -g serve
---
+++ scripts/start_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+serve -s /home/ec2-user/dist > /dev/null 2> /dev/null < /dev/null &
